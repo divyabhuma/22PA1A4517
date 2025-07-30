0571a667ea2290bfb1f84613efeec89bce648710
divyabhuma <157564757+divyabhuma@users.noreply.github.com>
2025-07-30 12:35:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-
-// Load environment variables
 dotenv.config();
-
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -24,20 +20,14 @@ const indexRouter = require('./routes/index');
 const shortUrlsRouter = require('./routes/shorturls');
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Routes
 app.use('/', indexRouter);
 app.use('/shorturls', shortUrlsRouter);
-
-// 404 handler
 app.use((req, res, next) => {
   next(createError(404));
 });
